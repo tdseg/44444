04693816d3bb0e94f8885f7b401afa016774c9a6
tdseg <96424025+tdseg@users.noreply.github.com>
2022-02-04 12:07:01 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Config xray
-UUID=ad806487-2d26-4636-98b6-ab85cc8521f7
+AUUID=ad806487-2d26-4636-98b6-ab85cc8521f7
 PORT=80
 rm -rf /etc/xray/config.json
 cat << EOF > /etc/xray/config.json
@@ -9,21 +9,56 @@ cat << EOF > /etc/xray/config.json
     "inbounds": 
     [
         {
-                "port": $PORT,
-			"protocol": "trojan",
+      "port": $PORT,
+            "protocol": "vmess",
             "settings": {
-			"clients": [
-			{
-			"password":"$AUUID"
-			}
-			]
-			},
+            "clients": [
+            {
+            "id": "$AUUID"
+            }
+            ]
+            },
             "streamSettings": {
-			"network": "ws",
-			"wsSettings": {
-			"path": "/trojan"
-			}
-			}
+            "network": "ws",
+            "wsSettings": {
+            "path": "/$AUUID-vmess"
+            }
+            }
+        },
+        {
+             "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+            "clients": [
+            {
+            "id": "$AUUID"
+            }
+            ],
+            "decryption": "none"
+            },
+            "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+            "path": "/$AUUID-vless"
+            }
+            }
+        },
+        {
+                "port": $PORT,
+            "protocol": "trojan",
+            "settings": {
+            "clients": [
+            {
+            "password":"$AUUID"
+            }
+            ]
+            },
+            "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+            "path": "/$AUUID-trojan"
+            }
+            }
         }
   ],
   "outbounds": [
@@ -37,3 +72,4 @@ EOF
 #run xray
 
 xray -c /etc/xray/config.json
+
