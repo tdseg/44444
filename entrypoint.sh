d5347dc8cc4c93a7cfe73a67d003dedf9f1ecffd
tdseg <96424025+tdseg@users.noreply.github.com>
2022-02-04 12:02:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,41 +9,6 @@ cat << EOF > /etc/xray/config.json
     "inbounds": 
     [
         {
-      "port": $PORT,
-			"protocol": "vmess",
-            "settings": {
-			"clients": [
-			{
-			"id": "$AUUID"
-			}
-			]
-			},
-            "streamSettings": {
-			"network": "ws",
-			"wsSettings": {
-			"path": "/$AUUID-vmess"
-			}
-			}
-        },
-        {
-             "port": $PORT,
-			"protocol": "vless",
-            "settings": {
-			"clients": [
-			{
-			"id": "$AUUID"
-			}
-			],
-			"decryption": "none"
-			},
-            "streamSettings": {
-			"network": "ws",
-			"wsSettings": {
-			"path": "/$AUUID-vless"
-			}
-			}
-        },
-        {
                 "port": $PORT,
 			"protocol": "trojan",
             "settings": {
@@ -56,7 +21,7 @@ cat << EOF > /etc/xray/config.json
             "streamSettings": {
 			"network": "ws",
 			"wsSettings": {
-			"path": "/$AUUID-trojan"
+			"path": "/trojan"
 			}
 			}
         }
